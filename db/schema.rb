1f1dc42bdef640f7681b93462d7426bb1c904e4e
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-05-13 12:55:58 +0100

    create homepage layout, community model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_172607) do
+ActiveRecord::Schema.define(version: 2021_05_13_114439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "communities", force: :cascade do |t|
+    t.string "string"
+    t.string "url"
+    t.text "rules"
+    t.integer "total_members"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_communities_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "bio"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_04_28_172607) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "communities", "users"
 end
